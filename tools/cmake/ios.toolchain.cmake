d682a88f9f284a5043e19ec930da703ec517b6f9
Yavor Georgiev <yavor.georgiev@outlook.com>
2020-04-13 20:18:42 +0200

    build and package

--- tools/cmake/ios.toolchain.cmake
+++ tools/cmake/ios.toolchain.cmake
@@ -5,17 +5,17 @@ check_generator("Xcode")
 set_common_xcode_attributes()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(REALM_SKIP_SHARED_LIB ON)
 set(REALM_BUILD_LIB_ONLY ON)
 
 set(CMAKE_SYSTEM_NAME iOS)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 8.0)
-set(CMAKE_IOS_INSTALL_COMBINED ON)
 set(CMAKE_OSX_ARCHITECTURES
     armv7
     i386
     arm64
     x86_64
 )
+
+set(CMAKE_XCODE_ATTRIBUTE_ENABLE_BITCODE[sdk=iphone*] "YES")
 
 set_bitcode_attributes()
